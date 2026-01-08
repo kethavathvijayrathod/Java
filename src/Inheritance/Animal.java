d3e0c5ebd87e952f263cf0a748c4ffd32d9f2eed
kethavathvijayrathod <kethavathvijay2210@gmail.com>
2026-01-08 17:05:00 +0530

    .

--- src/Inheritance/Animal.java
+++ src/Inheritance/Animal.java
@@ -1,5 +1,29 @@
 package Inheritance;
-
-public class Animal {
-
+class An {
+	void display() {
+		System.out.println("This is an animal" );
+	}
 }
+class Dog extends An{
+}
+public class Animal{
+	public static void main(String[] args) {
+		Dog d =new Dog();
+		d.display();
+	}
+}
+
+
+
+//class Dog {
+//	public void display(){
+//		System.out.println("This is an animal" );
+//	}
+//}
+//  public class Animal extends Dog{
+//	public static void main(String[] args) {
+//	Dog d =new Dog();
+//	d.display();
+//	}
+//	
+//}
